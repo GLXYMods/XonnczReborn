e8bb9c0590a13ea7fa15bbe0146159c3936c4b01
GLXYMods <gnmmodding123@gmail.com>
2024-12-16 18:44:23 -0500

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.GLXYMods.XonnczReborn";
+        public const string Name = "X O N N C Z";
+        public const string Description = "Created by @glxy";
+        public const string Version = "1.0.0";
+    }
+}
