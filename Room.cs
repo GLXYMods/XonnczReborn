649718bb123eaeab2ab4f9658cd394ca61b07f4c
GLXYMods <gnmmodding123@gmail.com>
2025-02-05 18:29:39 -0500

    Update Room.cs

--- Room.cs
+++ Room.cs
@@ -1,12 +1,68 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using GorillaNetworking;
 using Photon.Pun;
+using StupidTemplate.Classes;
+using StupidTemplate.Notifications;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static StupidTemplate.Menu.Main;
+using static StupidTemplate.Mods.Movement;
 
 namespace StupidTemplate.Mods
 {
     public class Room
     {
+
+        public static bool dis = false;
+        public static void AntiReport()
+        {
+            var scoreboardLine = GorillaScoreboardTotalUpdater.allScoreboardLines.Find((GorillaPlayerScoreboardLine L) => L.playerVRRig.isLocal);
+            foreach (VRRig vrrigs in GorillaParent.instance.vrrigs)
+            {
+                var Limit = 0.51f;
+                var ReportPosition = scoreboardLine.reportButton.gameObject.transform.position;
+                var RightDis = Vector3.Distance(vrrigs.rightHandTransform.position, ReportPosition);
+                var LeftDis = Vector3.Distance(vrrigs.leftHandTransform.position, ReportPosition);
+                if (RightDis <= Limit || LeftDis <= Limit)
+                {
+                    if (!vrrigs.isLocal && !vrrigs.isMyPlayer)
+                    {                        
+                        PhotonNetwork.Disconnect();
+                        flush();
+                    }
+                }
+            }
+        }
+
+
+        public static void CantMoveFingers()
+        {
+            ControllerInputPoller.instance.rightControllerGripFloat = 0f;
+            ControllerInputPoller.instance.leftControllerGripFloat = 0f;
+            ControllerInputPoller.instance.rightControllerIndexFloat = 0f;
+            ControllerInputPoller.instance.leftControllerIndexFloat = 0f;
+            ControllerInputPoller.instance.rightControllerPrimaryButton = false;
+            ControllerInputPoller.instance.leftControllerPrimaryButton = false;
+            ControllerInputPoller.instance.rightControllerPrimaryButtonTouch = false;
+            ControllerInputPoller.instance.leftControllerPrimaryButtonTouch = false;
+            ControllerInputPoller.instance.rightControllerSecondaryButtonTouch = false;
+            ControllerInputPoller.instance.leftControllerSecondaryButtonTouch = false;
+            ControllerInputPoller.instance.leftControllerSecondaryButton = false;
+            ControllerInputPoller.instance.rightControllerSecondaryButton = false;
+        }
+
+        
+
+        public static void LeaveTroop()
+        {
+            GorillaComputer.instance.LeaveTroop();
+        }
+
+
+
+
 
 
         public static void roominfo()
@@ -14,15 +70,16 @@ namespace StupidTemplate.Mods
             if (PhotonNetwork.InRoom == true)
             {
                 string name = PhotonNetwork.CurrentRoom.Name;
-                string idk = PhotonNetwork.CountOfPlayersInRooms.ToString();
                 string masterId = PhotonNetwork.CurrentRoom.MasterClientId.ToString();
-                Notifications.NotifiLib.SendNotification($"{name}, {masterId}, {idk}");
+                Notifications.NotifiLib.SendNotification($"Room Code : {name}");
             }
             else
             {
-                Notifications.NotifiLib.SendNotification("<color=red>{ERROR}</color>, Not Connected To A Room");
+                NotifiLib.SendNotification("<color=red>{ERROR}</color>, Not Connected To A Room");
             }
         }
+
+
         public static void DisconnectB()
         {
             if (PhotonNetwork.InRoom == true)
@@ -34,9 +91,11 @@ namespace StupidTemplate.Mods
             }
             else
             {
-                Notifications.NotifiLib.SendNotification("<color=red>{ERROR}</color>, Not Connected To A Room");
+                NotifiLib.SendNotification("<color=red>{ERROR}</color>, Not Connected To A Room");
             }
         }
+
+
         public static void DisconnectRT()
         {
             if (PhotonNetwork.InRoom == true)
@@ -51,6 +110,8 @@ namespace StupidTemplate.Mods
                 Notifications.NotifiLib.SendNotification("<color=red>{ERROR}</color>, Not Connected To A Room");
             }
         }
+
+
         public static void DisconnectLT()
         {
             if (PhotonNetwork.InRoom == true)
