9a862c5868592a2995c57e7d0d238572ce65be53
GLXYMods <gnmmodding123@gmail.com>
2025-05-29 23:07:37 -0400

    Update Rig.cs

--- Rig.cs
+++ Rig.cs
@@ -10,6 +10,7 @@ using StupidTemplate.Classes;
 using BepInEx;
 using UnityEngine.InputSystem;
 using Object = UnityEngine.Object;
+using Player = GorillaLocomotion.GTPlayer;
 using static StupidTemplate.Menu.Main;
 using StupidTemplate.Notifications;
 using StupidTemplate.Patches;
@@ -19,14 +20,61 @@ namespace StupidTemplate.Mods
     public class Rig
     {
 
+        public static int l = 0;
+
+        public static void ChangeLongArmsShit()
+        {
+            l++;
+            if (l < 0)
+            {
+                l = 4;
+            }
+            if (l > 4)
+            {
+                l = 0;
+            }
+            if (l == 0)
+            {
+                GetIndex("LongArm Size").overlapText = "Small";
+                longarmsize = new Vector3(1.1f, 1.1f, 1.1f);
+            }
+            if (l == 1)
+            {
+                GetIndex("LongArm Size").overlapText = "Medium";
+                longarmsize = new Vector3(1.2f, 1.2f, 1.2f);
+            }
+            if (l == 2)
+            {
+                GetIndex("LongArm Size").overlapText = "Large";
+                longarmsize = new Vector3(1.3f, 1.3f, 1.3f);
+            }
+            if (l == 3)
+            {
+                GetIndex("LongArm Size").overlapText = "Very Large";
+                longarmsize = new Vector3(1.4f, 1.4f, 1.4f);
+            }
+            if (l == 4)
+            {
+                GetIndex("LongArm Size").overlapText = "Massive?";
+                longarmsize = new Vector3(1.6f, 1.6f, 1.6f);
+            }
+        }
+
+        public static void LongArms()
+        {
+            GTPlayer.Instance.transform.localScale = longarmsize;
+        }
+
+        public static Vector3 longarmsize = new Vector3(1.3f, 1.3f, 1.3f);
+
         public static void GrabRig()
         {
-            RigPatch.Prefix(GorillaTagger.Instance.offlineVRRig);
-            RigPatch2.Prefix();
+            GhostPatch.Prefix(GorillaTagger.Instance.offlineVRRig);
+            GhostPatch2.Prefix(VRRigJobManager.Instance, GorillaTagger.Instance.offlineVRRig);
             if (ControllerInputPoller.instance.rightGrab)
             {
                 GorillaTagger.Instance.offlineVRRig.enabled = false;
-                GorillaTagger.Instance.offlineVRRig.transform.position = GorillaLocomotion.Player.Instance.rightControllerTransform.position;
+                GorillaTagger.Instance.offlineVRRig.transform.position = GorillaLocomotion.GTPlayer.Instance.rightControllerTransform.position;
             }
             else
             {
@@ -36,7 +84,7 @@ namespace StupidTemplate.Mods
             if (ControllerInputPoller.instance.leftGrab)
             {
                 GorillaTagger.Instance.offlineVRRig.enabled = false;
-                GorillaTagger.Instance.offlineVRRig.transform.position = GorillaLocomotion.Player.Instance.leftControllerTransform.position;
+                GorillaTagger.Instance.offlineVRRig.transform.position = GorillaLocomotion.GTPlayer.Instance.leftControllerTransform.position;
             }
             else
             {
@@ -46,7 +94,7 @@ namespace StupidTemplate.Mods
             if (ControllerInputPoller.instance.leftControllerIndexFloat > 0.5f)
             {
                 GorillaTagger.Instance.offlineVRRig.enabled = false;
-                GorillaTagger.Instance.offlineVRRig.transform.position = GorillaLocomotion.Player.Instance.leftControllerTransform.position;
+                GorillaTagger.Instance.offlineVRRig.transform.position = GorillaLocomotion.GTPlayer.Instance.leftControllerTransform.position;
             }
             else
             {
@@ -55,7 +103,7 @@ namespace StupidTemplate.Mods
             if (ControllerInputPoller.instance.rightControllerIndexFloat > 0.5f)
             {
                 GorillaTagger.Instance.offlineVRRig.enabled = false;
-                GorillaTagger.Instance.offlineVRRig.transform.position = GorillaLocomotion.Player.Instance.rightControllerTransform.position;
+                GorillaTagger.Instance.offlineVRRig.transform.position = GorillaLocomotion.GTPlayer.Instance.rightControllerTransform.position;
             }
             else
             {
@@ -63,21 +111,6 @@ namespace StupidTemplate.Mods
             }
         }
 
-        public static void SizeChanger()
-        {
-            if (ControllerInputPoller.instance.rightControllerPrimaryButton)
-            {
-                Player.Instance.scale = 1f;
-            }
-            if (ControllerInputPoller.instance.rightGrab)
-            {
-                Player.Instance.scale *= 0.05f;
-            }
-            if (ControllerInputPoller.instance.leftGrab)
-            {
-                Player.Instance.scale *= -0.05f;
-            }
-        }
 
         public static VRRig lockon;
         public static VRRig rigg;
@@ -85,11 +118,11 @@ namespace StupidTemplate.Mods
 
         public static void riggun()
         {
-            RigPatch.Prefix(GorillaTagger.Instance.offlineVRRig);
-            RigPatch2.Prefix();
+            GhostPatch.Prefix(GorillaTagger.Instance.offlineVRRig);
+            GhostPatch2.Prefix(VRRigJobManager.Instance, GorillaTagger.Instance.offlineVRRig);
             if (ControllerInputPoller.instance.rightGrab)
             {
-                Physics.Raycast(GorillaLocomotion.Player.Instance.rightControllerTransform.position, -GorillaLocomotion.Player.Instance.rightControllerTransform.up, out var hitInfo);
+                Physics.Raycast(GorillaLocomotion.GTPlayer.Instance.rightControllerTransform.position, -GorillaLocomotion.GTPlayer.Instance.rightControllerTransform.up, out var hitInfo);
                 point = GameObject.CreatePrimitive(PrimitiveType.Sphere);
                 point.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
                 point.GetComponent<Renderer>().material.shader = Shader.Find("GorillaTag/UberShader");
@@ -103,7 +136,7 @@ namespace StupidTemplate.Mods
                 lineRenderer2.endWidth = 0.01f;
                 lineRenderer2.positionCount = 2;
                 lineRenderer2.useWorldSpace = true;
-                lineRenderer2.SetPosition(0, GorillaLocomotion.Player.Instance.rightControllerTransform.position);
+                lineRenderer2.SetPosition(0, GorillaLocomotion.GTPlayer.Instance.rightControllerTransform.position);
                 lineRenderer2.SetPosition(1, point.transform.position);
                 lineRenderer2.material.shader = Shader.Find("GUI/Text Shader");
                 UnityEngine.Object.Destroy(lineRenderer2, Time.deltaTime);
@@ -128,12 +161,12 @@ namespace StupidTemplate.Mods
         }
         public static void riggun2()
         {
-            RigPatch.Prefix(GorillaTagger.Instance.offlineVRRig);
-            RigPatch2.Prefix();
+            GhostPatch.Prefix(GorillaTagger.Instance.offlineVRRig);
+            GhostPatch2.Prefix(VRRigJobManager.Instance, GorillaTagger.Instance.offlineVRRig);
             if (Mouse.current.rightButton.isPressed)
             {
                 RaycastHit raycastHit;
-                Ray ray = Camera.main.ScreenPointToRay(UnityInput.Current.mousePosition);
+                Ray ray = GameObject.Find("Shoulder Camera").activeSelf ? GameObject.Find("Shoulder Camera").GetComponent<Camera>().ScreenPointToRay(UnityInput.Current.mousePosition) : GorillaTagger.Instance.mainCamera.GetComponent<Camera>().ScreenPointToRay(UnityInput.Current.mousePosition);
                 if (Physics.Raycast(ray, out raycastHit) && point == null)
                 {
                     point = GameObject.CreatePrimitive(PrimitiveType.Sphere);
@@ -154,7 +187,7 @@ namespace StupidTemplate.Mods
                 lineRenderer.endWidth = 0.01f;
                 lineRenderer.positionCount = 2;
                 lineRenderer.useWorldSpace = true;
-                lineRenderer.SetPosition(0, GorillaLocomotion.Player.Instance.headCollider.transform.position);
+                lineRenderer.SetPosition(0, GorillaLocomotion.GTPlayer.Instance.headCollider.transform.position);
                 lineRenderer.SetPosition(1, point.transform.position);
                 lineRenderer.material.shader = Shader.Find("GUI/Text Shader");
                 UnityEngine.Object.Destroy(lineRenderer, Time.deltaTime);
@@ -186,13 +219,13 @@ namespace StupidTemplate.Mods
 
         public static void freeze()
         {
-            RigPatch.Prefix(GorillaTagger.Instance.offlineVRRig);
-            RigPatch2.Prefix();
+            GhostPatch.Prefix(GorillaTagger.Instance.offlineVRRig);
+            GhostPatch2.Prefix(VRRigJobManager.Instance, GorillaTagger.Instance.offlineVRRig);
             if (ControllerInputPoller.instance.rightGrab || Mouse.current.rightButton.isPressed)
             {
                 GorillaTagger.Instance.offlineVRRig.enabled = false;
-                GorillaTagger.Instance.offlineVRRig.transform.position = GorillaLocomotion.Player.Instance.headCollider.transform.position;
-                GorillaTagger.Instance.myVRRig.transform.position = GorillaLocomotion.Player.Instance.headCollider.transform.position;
+                GorillaTagger.Instance.offlineVRRig.transform.position = GorillaLocomotion.GTPlayer.Instance.headCollider.transform.position;
+                GorillaTagger.Instance.myVRRig.transform.position = GorillaLocomotion.GTPlayer.Instance.headCollider.transform.position;
             }
             else
             {
@@ -217,8 +250,8 @@ namespace StupidTemplate.Mods
         }
         public static void Ghost()
         {
-            RigPatch.Prefix(GorillaTagger.Instance.offlineVRRig);
-            RigPatch2.Prefix();
+            GhostPatch.Prefix(GorillaTagger.Instance.offlineVRRig);
+            GhostPatch2.Prefix(VRRigJobManager.Instance, GorillaTagger.Instance.offlineVRRig);
             bool Rp = ControllerInputPoller.instance.rightControllerPrimaryButton;
             if (!ghost && Rp || Mouse.current.rightButton.isPressed)
             {
@@ -232,14 +265,14 @@ namespace StupidTemplate.Mods
                 float h = (Time.frameCount / 180f) % 1f;
                 gameObject1.GetComponent<Renderer>().material.color = Color.Lerp(Color.red, Color.blue, Mathf.PingPong(Time.time, 1f));
                 gameObject1.transform.localScale = new Vector3(0.10f, 0.10f, 0.10f);
-                gameObject1.transform.position = GorillaLocomotion.Player.Instance.rightControllerTransform.position;
+                gameObject1.transform.position = GorillaLocomotion.GTPlayer.Instance.rightControllerTransform.position;
                 Object.Destroy(gameObject1.GetComponent<Rigidbody>());
                 Object.Destroy(gameObject1.GetComponent<Collider>());
                 Object.Destroy(gameObject1, Time.deltaTime);
                 GameObject gameObject2 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
                 gameObject2.GetComponent<Renderer>().material.color = Color.Lerp(Color.red, Color.blue, Mathf.PingPong(Time.time, 1f));
                 gameObject2.transform.localScale = new Vector3(0.10f, 0.10f, 0.10f);
-                gameObject2.transform.position = GorillaLocomotion.Player.Instance.leftControllerTransform.position;
+                gameObject2.transform.position = GorillaLocomotion.GTPlayer.Instance.leftControllerTransform.position;
                 Object.Destroy(gameObject2.GetComponent<Rigidbody>());
                 Object.Destroy(gameObject2.GetComponent<Collider>());
                 Object.Destroy(gameObject2, Time.deltaTime);
@@ -251,38 +284,47 @@ namespace StupidTemplate.Mods
                 GorillaTagger.Instance.offlineVRRig.mainSkin.material.shader = Shader.Find("GorillaTag/UberShader");
             }
         }
+
+
+
+        public static VRRig OverView;
         public static void Inviz()
         {
-            RigPatch.Prefix(GorillaTagger.Instance.offlineVRRig);
-            RigPatch2.Prefix();
+            GhostPatch.Prefix(GorillaTagger.Instance.offlineVRRig);
+            GhostPatch2.Prefix(VRRigJobManager.Instance, GorillaTagger.Instance.offlineVRRig);
             bool Rs = ControllerInputPoller.instance.rightControllerSecondaryButton;
-            if (!inviz && Rs || Mouse.current.leftButton.isPressed)
+            if (!inviz && Rs)
             {
                 inviz1 = !inviz1;
             }
-            inviz = Rs || Mouse.current.leftButton.isPressed;
+            inviz = Rs;
             if (inviz1)
             {
-                GorillaTagger.Instance.offlineVRRig.headBodyOffset = new Vector3(0f, -9999f, 0f);
+                GorillaTagger.Instance.offlineVRRig.headBodyOffset = new Vector3(0f, -999f, 0f);
                 GameObject gameObject1 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
                 float h = (Time.frameCount / 180f) % 1f;
                 gameObject1.GetComponent<Renderer>().material.color = Color.Lerp(Color.red, Color.blue, Mathf.PingPong(Time.time, 1f));
                 gameObject1.transform.localScale = new Vector3(0.10f, 0.10f, 0.10f);
-                gameObject1.transform.position = GorillaLocomotion.Player.Instance.rightControllerTransform.position;
+                gameObject1.transform.position = GorillaLocomotion.GTPlayer.Instance.rightControllerTransform.position;
                 Object.Destroy(gameObject1.GetComponent<Rigidbody>());
                 Object.Destroy(gameObject1.GetComponent<Collider>());
                 Object.Destroy(gameObject1, Time.deltaTime);
                 GameObject gameObject2 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
                 gameObject2.GetComponent<Renderer>().material.color = Color.Lerp(Color.red, Color.blue, Mathf.PingPong(Time.time, 1f));
                 gameObject2.transform.localScale = new Vector3(0.10f, 0.10f, 0.10f);
-                gameObject2.transform.position = GorillaLocomotion.Player.Instance.leftControllerTransform.position;
+                gameObject2.transform.position = GorillaLocomotion.GTPlayer.Instance.leftControllerTransform.position;
                 Object.Destroy(gameObject2.GetComponent<Rigidbody>());
                 Object.Destroy(gameObject2.GetComponent<Collider>());
                 Object.Destroy(gameObject2, Time.deltaTime);
+                Prefix(GorillaTagger.Instance.offlineVRRig);
             }
             else
             {
                 GorillaTagger.Instance.offlineVRRig.headBodyOffset = new Vector3(0f, 0f, 0f);
+            }
+            if (Keyboard.current.hKey.wasPressedThisFrame)
+            {
+                GorillaTagger.Instance.offlineVRRig.headBodyOffset = new Vector3(0f, -999f, 0f);
             }
         }
 
@@ -290,8 +332,8 @@ namespace StupidTemplate.Mods
         {
             if (ControllerInputPoller.instance.rightGrab)
             {
-                Player.Instance.rightControllerTransform.transform.position = GorillaLocomotion.Player.Instance.bodyCollider.transform.position;
-                Player.Instance.leftControllerTransform.transform.position = GorillaLocomotion.Player.Instance.bodyCollider.transform.position;            
+                GTPlayer.Instance.rightControllerTransform.transform.position = GorillaLocomotion.GTPlayer.Instance.bodyCollider.transform.position;
+                GTPlayer.Instance.leftControllerTransform.transform.position = GorillaLocomotion.GTPlayer.Instance.bodyCollider.transform.position;            
             }
         }
 
